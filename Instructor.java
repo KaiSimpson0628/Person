349be763729378b3e7a2f35fc3f64e449cc814ec
KaiSimpson0628 <60585848+KaiSimpson0628@users.noreply.github.com>
2022-02-07 13:38:20 -0500

    Create Instructor.java

---
+++ Instructor.java
@@ -0,0 +1,12 @@
+public class Instructor extends Person {
+private double salary;
+public Instructor(String name, int birth, double salary) 
+	{
+	super (name, birth);
+	this.salary = salary;
+	}
+	public String toString() 
+	{
+		return(super.toString() + " and is earning " + salary);
+	}
+}// close instructor extends Person
